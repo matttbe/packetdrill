56368b8f5786ecf905534bb281bc07d06380998e
Matthieu Baerts <matthieu.baerts@tessares.net>
2022-01-28 14:31:16 +0100

    run_all: capture packet per test
    This is useful for debug purposes.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- gtests/net/packetdrill/in_netns.sh
+++ gtests/net/packetdrill/in_netns.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Execute a subprocess in a network namespace
 
@@ -6,12 +6,22 @@ set -e
 
 readonly NETNS="ns-$(mktemp -u XXXXXX)"
 
+# last arg is the file name
+for i in "${@}"; do pkt="${i}"; done
+for i in "${@}"; do [[ "${i}" = "--ip_version="* ]] && ipv="${i#*=}"; done
+OUTPUT="${HOME}/${pkt}_${ipv}.pcap"
+PID=
+
 setup() {
 	ip netns add "${NETNS}"
 	ip -netns "${NETNS}" link set lo up
+	ip netns exec "${NETNS}" tcpdump -i any -s 150 -w "${OUTPUT}" &
+	PID=$!
+	sleep 1
 }
 
 cleanup() {
+	kill ${PID}
 	ip netns del "${NETNS}"
 }
 
